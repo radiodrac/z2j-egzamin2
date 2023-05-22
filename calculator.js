1cfb361d07e4fc0e4533fdd272e4dc60d3b7e66d
Urszula Chrzanowska <124902071+radiodrac@users.noreply.github.com>
2023-05-22 16:03:50 +0200

    added math functions, added division by 0 check to modulo function

--- calculator.js
+++ calculator.js
@@ -1,6 +1,30 @@
 const error = "Błąd! Należy podać liczbę.";
 let operand1, operand2, operator, result;
 
+function calcSum(operand1, operand2) {
+    return operand1 + operand2;
+}
+
+function calcSubtract(operand1, operand2) {
+    return operand1 - operand2;
+}
+
+function calcMultiply(operand1, operand2) {
+    return operand1 * operand2;
+}
+
+function calcDivide(operand1, operand2) {
+    return (
+        (operand2 === 0) ? "Nie dziel przez 0!" : operand1 / operand2
+    );
+}
+
+function calcModulo(operand1, operand2) {
+    return (
+        (operand2 === 0) ? "Nie dziel przez 0!" : operand1 % operand2
+    );
+}
+
 operand1 = +prompt("Podaj pierwszą liczbę");
 
 if (isNaN(operand1)) {
@@ -14,15 +38,15 @@ if (isNaN(operand1)) {
         alert(error);
 
     } else if (operator === "+") {
-        result = operand1 + operand2;
+        result = calcSum(operand1, operand2);
     } else if (operator === "-") {
-        result = operand1 - operand2;
+        result = calcSubtract(operand1, operand2);
     } else if (operator === "*") {
-        result = operand1 * operand2;
+        result = calcMultiply(operand1, operand2);
     } else if (operator === "/") {
-        result = (operand2 === 0) ? "Nie dziel przez 0!" : operand1 / operand2;
+        result = calcDivide(operand1, operand2);
     } else if (operator === "%") {
-        result = operand1 % operand2;
+        result = calcModulo(operand1, operand2);
     } else {
         result = "Nieobsługiwany operator";
     }
